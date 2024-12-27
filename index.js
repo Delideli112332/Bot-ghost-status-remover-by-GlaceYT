5ad840227d7da7d0dda3f34fe579494a7ed50457
Delideli112332 <michal01.deli01@gmail.com>
2024-12-27 01:56:04 +0100

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["RED BOYS", "sram"];
+const statusMessages = ["discord.gg/", "discord.gg/"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
